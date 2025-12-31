ddae6f7e53a49b193106ae2bf4fc648387ca5e12
wilma <mornlitoralis@gmail.com>
2025-12-31 08:08:00 +0700

    some changes

--- tugasphp/controller/edit.php
+++ tugasphp/controller/edit.php
@@ -1,4 +1,16 @@
 <?php
-    include '../config.php'
+    include '../config.php';
+    
+    if (isset($_POST['nim'])){
+        $nim = $_POST['nim'];
+        $nama = $_POST['nama'];
+        $jurusan = $_POST['jurusan'];
+        $angkatan = $_POST['angkatan'];
+    }    
+    $sql = "UPDATE mhs SET nama = '$nama', jurusan = '$jurusan', angkatan = '$angkatan'where nim='$nim'";
+
+    $conn->close();
+    
+
 
 ?>
