ec5acbce3d272b7710b2c16b15c015f0f9f07296
wilma <mornlitoralis@gmail.com>
2025-12-31 08:33:52 +0700

    some changes

--- tugasphp/controller/simpanData.php
+++ tugasphp/controller/simpanData.php
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($conn->query($sql) === TRUE) {
         echo "Data mahasiswa baru berhasil ditambahkan! Mengalihkan...";
-        header("Location: index.php?status=success");
+        header("Location: ../view/index.php?status=success");
         exit();
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
