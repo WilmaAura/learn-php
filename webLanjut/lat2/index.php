c5d8c230c081e86f3e2d64e975052d3e54ef483d
wilma <mornlitoralis@gmail.com>
2026-03-11 14:51:58 +0700

    add lat2

---
+++ webLanjut/lat2/index.php
@@ -0,0 +1,20 @@
+<?php
+    include "mhs.php";
+    include "matkul.php";
+   
+    $mhs_1 = new mhs();
+    $matkul_1 = new matkul();
+    $matkul_2 = new matkul();
+
+    $matkul_1->setData("A11.12345", "Pemrograman Berbasis Web");
+    $matkul_2->setData("A11.12346", "Pemrograman Web Lanjut");
+
+    $mhs_1->setData("A11.2024.15841", "Wilma Auraruna Khalif", [$matkul_1, $matkul_2]);
+
+    # Panggil var $nim dan $nama dari objek mhs_1
+    echo "<pre>";
+    print_r($mhs_1);
+    print_r($mhs_1->nim);
+    print_r($mhs_1->nama);
+    echo "</pre>";
+?>
